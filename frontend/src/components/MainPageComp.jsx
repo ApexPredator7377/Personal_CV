9ec65717f3060a20e61a3ceb91deb7f02811cf47
Nathan Morgenrood - 32674309 <Nathan333nm@gmail.com>
2021-12-07 00:43:35 +0200

    Added animated text and made a few refinements to the main (about) page.

--- frontend/src/components/MainPageComp.jsx
+++ frontend/src/components/MainPageComp.jsx
@@ -1,18 +1,12 @@
-import { sizeHeight } from "@mui/system";
 import React, { Component } from "react";
 import ReactImageAppear from 'react-image-appear';
+import TextAnimation from 'react-animate-text';
 
 export default class MainPageComp extends Component {
   constructor(props) {
     super(props);
-
-    this.state = this.initialState;
+    this.state = {};
   }
-
-  initialState = {
-
-  };
-
 
   render() {
     return (
@@ -23,40 +17,31 @@ export default class MainPageComp extends Component {
           <div class="column">
             <div class="card">
             <ReactImageAppear
-                className="div"
-                style={{
-                  height: this.imageHeight,
-                  width: this.imageWidth,
-                  aspectRatio: 16 / 9,
-                }} 
                 src="https://firebasestorage.googleapis.com/v0/b/cv-nathan.appspot.com/o/Me.jpeg?alt=media&token=ad3802a8-4316-426d-a076-e1609f9c5956"
+                className="my-image"
                 loaderStyle={{ height: "150px",
                                width: "150px" }}
                 loader="https://firebasestorage.googleapis.com/v0/b/cv-nathan.appspot.com/o/Dual%20Ball-1s-200px.gif?alt=media&token=6a4e8e2c-09d1-4606-b72b-0616dccff423"
                 animation="zoomIn"
                 animationDuration="1s"
             />
-            {/* <img
-                className="div"
-                style={{
-                  height: this.imageHeight,
-                  width: this.imageWidth,
-                  aspectRatio: 16 / 9,
-                }}
-                src={
-                  this.state.viewImage ||
-                  "https://firebasestorage.googleapis.com/v0/b/cv-nathan.appspot.com/o/Me.jpeg?alt=media&token=ad3802a8-4316-426d-a076-e1609f9c5956"
-                }
-              /> */}
             </div>
             <div class="container">
         <h2>Nathan Morgenrood</h2>
-        <p class="title">Software developer</p>
-        <p>Describes me ...</p>
-        <p>Nathan333nm@gmail.com</p>
+        <TextAnimation charInterval="40" ><p class="title">Aspiring Software Developer</p>
+        <p>I am currently a final-year BSc Information Technology/Computer 
+          Science student at the North-West University. I define myself as being hardworking and motivated. 
+          I have a drive to continue evolving and becoming better in all aspects of my life. I have a thirst for 
+          fast paced knowledge and I’m very ambitious in an uncapped environment. I have a love for anything that can conduct 
+          electricity and a passion for anything that controls the flow of it. This passion does not end where the capabilities of 
+          hardware and software ends and continues into mechanics, especially those involving two and four wheels. I also find the 
+          mechanics of the human body intriguing and love to improve not only the performance of my mind, but also that of my body. 
+          This makes me anything, from a technophile to petrolhead to gymaholic and everything in-between and closely related. I love 
+          working alone and, in a team, and have adapted to an agile environment. I function most optimal under pressure and adapt 
+          fast to new skills and roles.  </p></TextAnimation>
       </div>
           </div>
         </div></>
-    );
+    )
   }
 }
